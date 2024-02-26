a9792002aab0685d46a8533963511c53c4988e35
prabhask5 <prabhask@berkeley.edu>
2024-02-26 00:39:59 +0000

    WIP syscall argument validation

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -5,15 +5,81 @@
 #include "threads/thread.h"
 #include "threads/synch.h"
 #include "userprog/process.h"
+#include "threads/vaddr.h"
 
 // Global syscall lock
 struct lock syscall_lock;
 
 static void syscall_handler(struct intr_frame*);
+static bool is_valid_addr(void* addr);
+static bool syscall_validate_word(void* word);
+static bool syscall_validate_ptr(void** ptr);
+static bool syscall_validate_str(char* str);
+static size_t syscall_validate_buffer(void* buffer, size_t size);
 
 void syscall_init(void) {
   lock_init(&syscall_lock);
   intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
+}
+
+/*
+  Validates the address for one byte of memory
+*/
+static bool is_valid_addr(void* addr) {
+  return is_user_vaddr(addr) && addr > 0 &&
+         pagedir_get_page(thread_current()->pcb->pagedir, addr) != NULL;
+}
+
+/*
+ Must check for null pointers, invalid pointers (e.g. pointing to unmapped memory), 
+ and illegal pointers (e.g. pointing to kernel memory). Beware: a 4-byte memory region 
+ (e.g. a 32-bit integer) may consist of 2 bytes of valid memory and 2 bytes of invalid 
+ memory, if the memory lies on a page boundary. You should handle these cases by 
+ terminating the user process
+*/
+static bool syscall_validate_word(void* word) {
+  // In general this validates 4 bytes of data (ptr, int, etc.)
+  // Check first bytes of ptr and last bytes of ptr
+  if (word == NULL || !is_valid_addr(word) || !is_valid_addr(word + sizeof(uint32_t) - 1)) {
+    return false;
+  }
+  return true;
+}
+
+static bool syscall_validate_ptr(void** ptr) {
+  // In general this validates 4 bytes of data (ptr, int, etc.)
+  // Check first bytes of ptr and last bytes of ptr
+  if (!syscall_validate_word(ptr))
+    return false;
+  if (!is_valid_addr(*ptr))
+    return false;
+  return true;
+}
+
+/* Takes in a char* and validates all memory from str[0] to the first null pointer */
+static bool syscall_validate_str(char* str) {
+  int i = 0;
+  do {
+    if (!is_valid_addr(str + i))
+      return false;
+    ++i;
+  } while (str[i] != "\0");
+  return true;
+}
+
+/* 
+Validates all memory from start of buffer to the end of the buffer
+(i.e. all memory between buffer[0] and buffer[size-1])
+If the valid buffer size is less than the size_t then
+
+TODO checkt to see if we can get away with only checking the first and last element
+*/
+static size_t syscall_validate_buffer(void* buffer, size_t size) {
+  for (size_t i = 0; i < size; i++) {
+    if (!is_valid_addr(buffer + i))
+      return i;
+  }
+  return size;
 }
 
 static void syscall_handler(struct intr_frame* f UNUSED) {
@@ -54,13 +120,41 @@ static void syscall_handler(struct intr_frame* f UNUSED) {
 
   /* printf("System call number: %d\n", args[0]); */
 
+  // Verify syscall id is located in valid user memory
+  if (!syscall_validate_ptr(&args[0])) {
+    kill(f);
+    return;
+  }
+
   if (args[0] == SYS_EXIT) {
+    if (!syscall_validate_ptr(&args[1])) {
+      kill(f);
+      return;
+    }
     // TODO Update child process exit status
     f->eax = args[1];
     process_exit(args[1]);
-  } else if (args[0] == SYS_PRACTICE) {
+  }
+
+  else if (args[0] == SYS_PRACTICE) {
+    if (!syscall_validate_ptr(&args[1])) {
+      kill(f);
+      return;
+    }
     f->eax = args[1] + 1;
-  } else if (args[0] == SYS_WRITE) {
+  }
+
+  else if (args[0] == SYS_WRITE) {
+
+    // Verify buffer pointer and size are in valid user memory
+    // fd, buffer, size
+    if (!syscall_validate_ptr(&args[1]) || !syscall_validate_ptr(&args[2]) ||
+        !syscall_validate_ptr(&args[3])) {
+      kill(f);
+      return;
+    }
+    int size = syscall_validate_buffer(args[2], args[3]);
+
     lock_acquire(&syscall_lock);
     uint32_t bytes_written = 0;
     struct list* fdt = &thread_current()->pcb->fdt;
@@ -80,8 +174,8 @@ static void syscall_handler(struct intr_frame* f UNUSED) {
       // TODO generalize this to more file descriptors
       if (strcmp(fd->name, "stdout") == 0 || strcmp(fd->name, "stderr") == 0) {
         // TODO breakup large buffers
-        putbuf(args[2], args[3]);
-        bytes_written = args[3];
+        putbuf(args[2], size);
+        bytes_written = size;
       }
     }
     // Return number of bytes written
